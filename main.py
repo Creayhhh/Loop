c643804d11c1436fd82a8eabc99d5040f8c8d926
Creay <1017354253@qq.com>
2023-01-14 20:12:52 +0800

    删掉了一些没有用的东西

--- main.py
+++ main.py
@@ -6,6 +6,5 @@ from login_page import LoginWindow
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     login_ui = LoginWindow()
-    # index_ui.showFullScreen()
     login_ui.show()
     sys.exit(app.exec())
